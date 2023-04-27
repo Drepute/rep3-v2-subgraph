c8bb784167cb1a8018bde9e01ae25cbc1f5ee531
Jashan Shetty <43118727+eth-jashan@users.noreply.github.com>
2023-04-27 16:43:37 +0530

    Tier Mint Entities

--- src/pod-factory.ts
+++ src/pod-factory.ts
@@ -1,10 +1,18 @@
 // import { BigInt } from "@graphprotocol/graph-ts";
 import { PodDeployed } from "../generated/PodFactory/PodFactory";
 import { CredentialDeployed } from "../generated/templates/Pod/Pod";
-import { DiamondCut } from "../generated/templates/DiamondCutFacet/DiamondCutFacet";
-// import { DiamondCut } from "../generated/templates/DiamondCutFacet/DiamondCutFacet";
-import { Credential, Pod } from "../generated/schema";
-import { DiamondCutFacet, Pod as PodTemplate } from "../generated/templates";
+import { Credential, Pod, TierNFt } from "../generated/schema";
+import {
+  Pod as PodTemplate,
+  TierMintFacet as TierMintTemplate,
+} from "../generated/templates";
+import { DiamondLoupeFacet } from "../generated/templates/DiamondLoupeFacet/DiamondLoupeFacet";
+import { Address, Bytes } from "@graphprotocol/graph-ts";
+import {
+  TierMint,
+  Transfer,
+  TierMintFacet as TierMintContract,
+} from "../generated/templates/TierMintFacet/TierMintFacet";
 
 export function handlePodDeployed(event: PodDeployed): void {
   let pod = Pod.load(event.params.proxy);
@@ -20,24 +28,74 @@ export function handlePodDeployed(event: PodDeployed): void {
 
 export function handleCredentialDeployed(event: CredentialDeployed): void {
   let credentials = Credential.load(event.params.credential);
-    if (!credentials) {
-      let credentials = new Credential(event.params.credential);
-      credentials.parentPod = event.address;
-      credentials.facets = [];
-      DiamondCutFacet.create(event.address);
-      credentials.save();
-    }
+  if (!credentials) {
+    let credentials = new Credential(event.params.credential);
+    credentials.parentPod = event.address;
 
-}
+    // create data source
+    TierMintTemplate.create(event.params.credential);
 
-export function handleDiamondCut(event: DiamondCut): void {
-  let credentials = Credential.load(event.address);
-  if (credentials) {
-    let facets = credentials.facets;
-    for(let i = 0; i<= event.params._diamondCut.length; i++){
-      facets.push(event.params._diamondCut[i].facetAddress)
-    }
-    credentials.facets = facets
+    const diamondLoupeFacet = DiamondLoupeFacet.bind(event.params.credential);
+    credentials.facets = diamondLoupeFacet
+      .facetAddresses()
+      .map<Bytes>((x: Bytes) => x);
     credentials.save();
   }
 }
+
+export function handleTierMintTransfer(event: Transfer): void {
+  let tierMintFacet = TierMintContract.bind(event.address);
+  let tierNft = TierNFt.load(`${event.transaction.hash}${event.params.id}`);
+  if (!tierNft) {
+    tierNft = new TierNFt(`${event.transaction.hash}${event.params.id}`);
+    tierNft.claimer = event.params.to;
+    tierNft.blockTimestamp = event.block.timestamp;
+    tierNft.credential = event.address;
+    let tierDataResult = tierMintFacet.try_getTokenUri(event.params.id);
+    if (tierDataResult.reverted) {
+      tierNft.metadataUri = "";
+    } else {
+      tierNft.metadataUri = tierMintFacet.getTokenUri(event.params.id);
+    }
+    let metaDataResult = tierMintFacet.try_getTierOfToken(event.params.id);
+    if (metaDataResult.reverted) {
+      tierNft.tier = 0;
+    } else {
+      tierNft.tier = tierMintFacet.getTierOfToken(event.params.id);
+    }
+    tierNft.tokenId = event.params.id;
+    tierNft.txHash = event.transaction.hash;
+    tierNft.save();
+  }
+}
+
+// export function handleTierMint(event: TierMint): void {
+//   let tierNft = TierNFt.load(`${event.transaction.hash}${event.params.tokenId}`);
+//   if(!tierNft){
+//     tierNft = new TierNFt(`${event.transaction.hash}${event.params.tokenId}`);
+//     tierNft.claimer = Address.fromI32(0);
+//     tierNft.blockTimestamp = event.block.timestamp;
+//     tierNft.credential = event.address;
+//     tierNft.tier = event.params.tier;
+//     tierNft.metadataUri = '';
+//     tierNft.tokenId = event.params.tokenId;
+//     tierNft.txHash = event.transaction.hash;
+//     tierNft.save();
+//   }
+// }
+
+// export function handlet(event: TierMint): void {
+//   let tierNft = TierNFt.load(
+//     `${event.transaction.hash}${event.params.tokenId}`
+//   );
+//   if (!tierNft) {
+//     tierNft = new TierNFt(`${event.transaction.hash}${event.params.tokenId}`);
+//     tierNft.claimer = Address.fromI32(0);
+//     tierNft.blockTimestamp = event.block.timestamp;
+//     tierNft.credential = event.address;
+//     tierNft.tier = event.params.tier;
+//     tierNft.metadataUri = "";
+//     tierNft.tokenId = event.params.tokenId;
+//     tierNft.save();
+//   }
+// }
